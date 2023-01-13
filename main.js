0a89d29cbd3e065a3c09fc51c5a5311a400c18d0
belfinbaby555 <105835749+belfinbaby555@users.noreply.github.com>
2023-01-13 20:01:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ function public(){
     document.getElementById("private").style.color="grey";
     document.getElementById("float-line").style.marginLeft="15%";
     document.getElementById("float-line").style.width="75px";
-    document.getElementById("public-nogroup").style.display="inherit";
+    document.getElementById("public-nogroup").style.display="none";
     document.getElementById("private-joingroup").style.display="none";
     document.getElementById("private-nogroups").style.display="none";
-    document.getElementById("public-groups").style.display="none";
+    document.getElementById("public-groups").style.display="inherit";
+    document.getElementById("private-groups").style.display="none";
 
 }
 function private(){
@@ -16,8 +17,9 @@ function private(){
     document.getElementById("float-line").style.width="85px";
     document.getElementById("public-nogroup").style.display="none";
     document.getElementById("private-joingroup").style.display="none";
-    document.getElementById("private-nogroups").style.display="inherit";
+    document.getElementById("private-nogroups").style.display="none";
     document.getElementById("public-groups").style.display="none";
+    document.getElementById("private-groups").style.display="inherit";
 
 }
 function join(){
@@ -25,14 +27,16 @@ function join(){
     document.getElementById("private-joingroup").style.display="inherit";
     document.getElementById("private-nogroups").style.display="none";
     document.getElementById("public-groups").style.display="none";
+    document.getElementById("private-groups").style.display="none";
 }
 function chat(){
     document.getElementById("chat").style.color="black";
     document.getElementById("people").style.color="rgb(165, 165, 165)";
     document.getElementById("float-line2").style.marginLeft="31.8%";
     document.getElementById("float-line2").style.width="85px";
-    document.getElementById("people-online").style.display="none";
-    document.getElementById("chats").style.display="inherit";
+    document.getElementById("people-online-none").style.display="none";
+    document.getElementById("chats").style.display="none";
+    document.getElementById("group-chats").style.display="inherit";
 }
 function people(){
     document.getElementById("chat").style.color="rgb(165, 165, 165)";
@@ -40,7 +44,8 @@ function people(){
     document.getElementById("float-line2").style.marginLeft="43.8%";
     document.getElementById("float-line2").style.width="95px";
     document.getElementById("chats").style.display="none";
-    document.getElementById("people-online").style.display="inherit";
+    document.getElementById("people-online-none").style.display="inherit";
+    document.getElementById("group-chats").style.display="none";
 }
 function cancel(){
     document.getElementById("group-available").style.display="none";
@@ -55,6 +60,11 @@ function joingroup(){
     document.getElementById("private-joingroup").style.display="none";
     document.getElementById("private-nogroups").style.display="none";
     document.getElementById("public-groups").style.display="inherit";
+    document.getElementById("private-groups").style.display="none";
     document.getElementById("group-available").style.display="none";
     document.getElementById("full").style.filter="none";
+}
+function group(){
+    document.querySelectorAll("group-info").style.cssText="background-color:red;border-right:4px solid black;color:white;"
+    document.getElementById("group-bio").style.cssText="color:white;"
 }
